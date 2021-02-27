6a4f3f9eb9996d37dc5d5b76fc87f3a37d3ecf46
Orxan <orxanyrz@mail.ru>
2021-02-27 12:44:12 +0400

    add new lines

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Orxan");
         System.out.println("Salam Orxan");
+        System.out.println("HEllo world");
     }
 
 }
